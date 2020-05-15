a333fa4fb305d00db3f923d4c9fa537588fb26b0
Yujie wU <ywu3-6>
2020-05-16 00:39:57 +1000

    create list customers feature

---
+++ src/com/allen/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.allen.springdemo.dao;
+
+import java.util.List;
+import com.allen.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+	
+}
---
+++ src/com/allen/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,38 @@
+package com.allen.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.allen.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory, name should match the bean id
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		// execute the query
+		List<Customer> customers = theQuery.getResultList();
+		
+		// return the results
+		return customers;
+	}
+
+}
